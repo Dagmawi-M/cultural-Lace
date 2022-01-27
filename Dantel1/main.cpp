f10aa3071f9ae4b2e6b0c3b83a89c2be347cbf14
Dagmawi-M <43979542+Dagmawi-M@users.noreply.github.com>
2022-01-27 20:13:39 +0300

    Modularized some components.

--- Dantel1/main.cpp
+++ Dantel1/main.cpp
@@ -6,27 +6,22 @@
 #include <algorithm>
 
 
-int radius= 100 , smallradius=20 ,stroke=115 , smallstroke= 22 , MinicircleRadius=3;
+int radius= 100 , smallradius=20 ,stroke=115 , smallstroke= 22 , MinicircleRadius=3,Cx=267,Cy=270;
+int p1x=185,p1y=350,p2x=267,p2y=350,p3x=350,p3y=350,p4x=350,p4y=270,p5x=350,p5y=190,p6x=267,p6y=190,p7x=185,p7y=190,p8x=185,p8y=270;
 
 void init(void) {
     glClearColor(0.0,0.0,0.0,1);
     glMatrixMode(GL_PROJECTION);
+    glMatrixMode(GL_MODELVIEW);
     gluOrtho2D(0.0,500.0,0.0,500.0);
 }
 
-//void myEllipse(int x,int y,float StAngle,float EndAngle,int RX, int RY)
-//{
-//	double i;
-//	glBegin(GL_LINE_STRIP);
-//	glColor3f(0,1,0);
-//	i=StAngle;
-//	while(i<=EndAngle)
-//	{
-//		glVertex2f(int((RX*cos(i)+y)+.5),int((RY*sin(i)+x)+.5));
-//		i=i+.001;
-//	}
-//		glEnd();
-//}
+void initRendering()
+{
+glEnable(GL_DEPTH_TEST);
+}
+
+
 void myLineEllipse(int x,int y,float StAngle,float EndAngle,int RX, int RY)
 {
 	double i;
@@ -35,7 +30,7 @@ void myLineEllipse(int x,int y,float StAngle,float EndAngle,int RX, int RY)
 	i=StAngle;
 	while(i<=EndAngle)
 	{
-		glVertex2f(int((RX*cos(i)+y)+.5),int((RY*sin(i)+x)+.5));
+		glVertex2f(int((RX*cos(i)+x)+.5),int((RY*sin(i)+y)+.5));
 		i=i+.001;
 	}
 		glEnd();
@@ -49,7 +44,7 @@ void myEllipse(int x,int y,float StAngle,float EndAngle,int RX, int RY)
 	i=StAngle;
 	while(i<=EndAngle)
 	{
-		glVertex2f(int((RX*cos(i)+y)+.5),int((RY*sin(i)+x)+.5));
+		glVertex2f(int((RX*cos(i)+x)+.5),int((RY*sin(i)+y)+.5));
 		i=i+.001;
 	}
 		glEnd();
@@ -82,16 +77,8 @@ glColor3f(1,1,1);
 }
 
 
-
-void EllipseLace(int y, int x, int Rx ,int Ry ){
-        glColor3f(1,1,1);
-         myEllipse(y , x ,0, 180 , Rx,Ry );// Stroke
-
-         glColor3f(0.35,0.35, 0.67);
-         myEllipse(y , x ,0, 180 , Rx-10,Ry-10 ); // Ellipse
-     }
-
 void DrawLine(int x, int y, int a, int b){
+     glColor3f(0.8,0.8,0.8);
     glBegin(GL_LINES);
         glVertex2i(x,y);
         glVertex2i(a,b);
@@ -99,6 +86,27 @@ void DrawLine(int x, int y, int a, int b){
     glFlush();
 }
 
+void DrawFold(int Ex, int Ey,int orientation){
+     if (orientation=='H'){
+        DrawLine(Cx,Cy,(Cx-((Cx-Ex)*2)),Cy);
+        DrawLine(Cx,Cy+1,(Cx-((Cx-Ex)*2)),Cy);}
+         else if (orientation=='V'){
+         DrawLine(Cx,Cy,Cx,(Cy-((Cy-Ey)*2)));
+         DrawLine(Cx+1,Cy,Cx,(Cy-((Cy-Ey)*2)));}
+}
+
+
+void EllipseLace(int y, int x, int Rx ,int Ry){
+
+
+        glColor3f(1,1,1);
+         myEllipse(y , x ,0, 180 , Rx,Ry );// Stroke
+
+         glColor3f(0.35,0.35, 0.67);
+         myEllipse(y , x ,0, 180 , Rx-10,Ry-10 ); // Ellipse
+
+
+}
 
 void CircleLace(int radius,int stroke, int x , int y , char type){
 
@@ -122,75 +130,89 @@ void CircleLace(int radius,int stroke, int x , int y , char type){
             DrawCircle(radius,x,y);//Draw Tiny Circles
                 break;
      }
-
-
-
-
-    EllipseLace(270,185,90,40);//Left
-    EllipseLace(270,350,90,40);//Right
-    EllipseLace(350,267 ,40,90);//Top
-    EllipseLace(190,267,40,90);//Bottom
-
-
-    glColor3f(0.8,0.8,0.8);
-    DrawLine(267,270,104,270);//Left
-    DrawLine(267,271,104,271);
-
-    DrawLine(267,270,430,270);//Right
-    DrawLine(267,271,430,271);
-
-    DrawLine(267,270,267,430);//Top
-    DrawLine(267,271,268,430);
-
-    DrawLine(267,270,267,110);//Bottom
-    DrawLine(267,271,268,110);
-
-     //*****************************************************
-      glColor3f(1,1,1); //white stroke For large and small circles
-    DrawCircle(smallstroke,267,270);
-     glColor3f(0.8,0.8,0.8); //Grey For Emboss look
-        DrawCircle(smallradius,267,270); // Draws small circle
-            glColor3f(0.35,0.35, 0.67); // violet-blue for Tiny Circles
-            DrawCircle(MinicircleRadius,267,270);//Draw Tiny Circles
-
-//CircleLace(smallradius, smallstroke, 267,270, 'S');
-    // myEllipse(270 , 185 ,0, 180 , 100,50 );
 }
+
 
 
 void Dantel(){
 
-CircleLace(radius, stroke, 185, 350 , 'L'); //LeftTopCircle + stroke + Pattern
-CircleLace(radius, stroke, 185, 190 , 'L'); //LeftBottomCircle + stroke + Pattern
-CircleLace(radius, stroke, 350, 350 , 'L'); //RightTopCircle + stroke + Pattern
-CircleLace(radius, stroke, 350, 190, 'L'); //RightBottomCircle + stroke + Pattern
+//CircleLace(radius, stroke, 185, 350 , 'L'); //LeftTopCircle + stroke + Pattern
+//CircleLace(radius, stroke, 185, 190 , 'L'); //LeftBottomCircle + stroke + Pattern
+//CircleLace(radius, stroke, 350, 350 , 'L'); //RightTopCircle + stroke + Pattern
+//CircleLace(radius, stroke, 350, 190, 'L'); //RightBottomCircle + stroke + Pattern
+//
+//EllipseLace(185,270,90,40);//Left
+//EllipseLace(350,270,90,40);//Right
+//EllipseLace(267,350 ,40,90);//Top
+//EllipseLace(267,190,40,90);//Bottom
+//
+//DrawFold(185,270,'H');//Folded illusion Line
+//DrawFold(350,270,'H');
+//DrawFold(267,350,'V');
+//DrawFold(267,190,'V');
+//
+//CircleLace(smallradius, smallstroke, 185,350, 'S'); //(S)SmallRightBottomCircle + stroke
+//CircleLace(smallradius, smallstroke, 185,190, 'S'); //(S)SmallRightBottomCircle + stroke
+//CircleLace(smallradius, smallstroke, 350,350, 'S'); //(S)SmallRightBottomCircle + stroke
+//CircleLace(smallradius, smallstroke, 350,190, 'S'); //(S)SmallRightBottomCircle + stroke
+//
+//
+//CircleLace(MinicircleRadius, 0, 185,350, 'T'); //(T)TinyRightBottomCircle
+//CircleLace(MinicircleRadius, 0, 185,190, 'T'); //(T)TinyRightBottomCircle
+//CircleLace(MinicircleRadius, 0, 350,350, 'T'); //(T)TinyRightBottomCircle
+//CircleLace(MinicircleRadius, 0, 350,190, 'T'); //(T)TinyRightBottomCircle
+//
+//
+//CircleLace(smallradius, smallstroke, 267,270, 'S'); //Small Center Circle
+//CircleLace(MinicircleRadius, 0, 267,270, 'T'); //(T)TinyRightBottomCircleforCenter
+CircleLace(radius, stroke, p1x, p1y , 'L'); //LeftTopCircle + stroke + Pattern
+CircleLace(radius, stroke, p7x, p7y , 'L'); //LeftBottomCircle + stroke + Pattern
+CircleLace(radius, stroke, p3x, p3y , 'L'); //RightTopCircle + stroke + Pattern
+CircleLace(radius, stroke, p5x, p5y, 'L'); //RightBottomCircle + stroke + Pattern
+
+EllipseLace(p8x, p8y,90,40);//Left
+EllipseLace(p4x, p4y,90,40);//Right
+EllipseLace(p2x, p2y ,40,90);//Top
+EllipseLace(p6x, p6y,40,90);//Bottom
+
+DrawFold(p8x, p8y,'H');//Folded illusion Line
+DrawFold(p4x, p4y,'H');
+DrawFold(p2x, p2y ,'V');
+DrawFold(p6x, p6y,'V');
+
+CircleLace(smallradius, smallstroke, p1x, p1y, 'S'); //(S)SmallTopLeftCircle + stroke
+CircleLace(smallradius, smallstroke, p7x, p7y, 'S'); //(S)SmallBottomLeftCircle + stroke
+CircleLace(smallradius, smallstroke, p3x, p3y, 'S'); //(S)SmallTopRightCircle + stroke
+CircleLace(smallradius, smallstroke, p5x, p5y, 'S'); //(S)SmallBottomRightCircle + stroke
 
 
-CircleLace(smallradius, smallstroke, 185,350, 'S'); //(S)SmallRightBottomCircle + stroke
-CircleLace(smallradius, smallstroke, 185,190, 'S'); //(S)SmallRightBottomCircle + stroke
-CircleLace(smallradius, smallstroke, 350,350, 'S'); //(S)SmallRightBottomCircle + stroke
-CircleLace(smallradius, smallstroke, 350,190, 'S'); //(S)SmallRightBottomCircle + stroke
- //(S)SmallCircleforcenter + stroke
+CircleLace(MinicircleRadius, 0, p1x, p1y, 'T'); //(T)TinyRightBottomCircle
+CircleLace(MinicircleRadius, 0, p7x, p7y, 'T'); //(T)TinyRightBottomCircle
+CircleLace(MinicircleRadius, 0, p3x, p3y, 'T'); //(T)TinyRightBottomCircle
+CircleLace(MinicircleRadius, 0, p5x, p5y, 'T'); //(T)TinyRightBottomCircle
 
 
-CircleLace(MinicircleRadius, 0, 185,350, 'T'); //(T)TinyRightBottomCircle
-CircleLace(MinicircleRadius, 0, 185,190, 'T'); //(T)TinyRightBottomCircle
-CircleLace(MinicircleRadius, 0, 350,350, 'T'); //(T)TinyRightBottomCircle
-CircleLace(MinicircleRadius, 0, 350,190, 'T'); //(T)TinyRightBottomCircle
-CircleLace(MinicircleRadius, 0, 267,270, 'T'); //(T)TinyRightBottomCircleforCenter
-//glColor3f(0,1,0);
-//DrawEllipse(100,270,50,60,10);
-
+CircleLace(smallradius, smallstroke, Cx,Cy, 'S'); //Small Center Circle
+CircleLace(MinicircleRadius, 0, Cx,Cy, 'T'); //(T)TinyRightBottomCircleforCenter
 
 }
+
+
+
 
 int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500,500);
     glutInitWindowPosition(0,0);
+
     glutCreateWindow("Dantel");
+
     init();
     glutDisplayFunc(Dantel);
+
+
+
+
     glutMainLoop();
 }
